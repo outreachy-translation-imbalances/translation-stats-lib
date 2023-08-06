6fd34c006622a0aec0abb06c4b25df93212ba303
Adam Wight <adam.wight@wikimedia.de>
2023-08-05 17:48:54 -0700

    Reuse database logic
    Also makes an opinionated change, to fail fast on any type of database
    error.  This is probably better than continuing with missing data, and
    once the data table calculations are fully memoized it will be less
    expensive to retry after failure.

--- translation_stats/metawiki_translator_language_proficiency_standard_babel.py
+++ translation_stats/metawiki_translator_language_proficiency_standard_babel.py
@@ -1,68 +1,23 @@
-import pymysql
 import csv
-import socket
 import os
 from .data_store import cached
-
-
-def make_connection(wiki, replica_type="analytics"):
-    """Connects to a host and database of the same name.
-    
-    `replica_type` can be either "analytics" (default) or "web"."""
-    assert replica_type in ("analytics", "web")
-
-    try:
-        connection = pymysql.connect(
-            host=f"{wiki}.{replica_type}.db.svc.wikimedia.cloud",
-            read_default_file="~/.my.cnf",
-            database=f"{wiki}_p",
-            charset='utf8'
-        )
-        return connection
-    except socket.gaierror as e:
-        error_msg = f"Hostname resolution failed: {e}"
-        print(error_msg)
-        raise pymysql.Error(error_msg)
-    except pymysql.Error as e:
-        error_msg = f"Error occurred while connecting to the database: {e}"
-        print(error_msg)
-        raise pymysql.Error(error_msg)
-
-
-def query(conn, query):
-    """Execute a SQL query against the connection, and return **all** the results."""
-    with conn.cursor() as cur:
-        cur.execute(query)
-        data = cur.fetchall()
-        return data
+from .wiki_replica import query
 
 
 def fetch_babel_data(database):
-    try:
-        conn = make_connection(database)
-        results = query(
-            conn,
-            """
-            SELECT
-                DISTINCT a.actor_name AS username,
-                b.babel_lang as language_used,
-                b.babel_level as language_level
-            FROM actor a
-            LEFT JOIN babel b ON b.babel_user = a.actor_user
-            WHERE b.babel_user is not null
-            ORDER BY a.actor_name
-            """
-        )
-        conn.close()
-        return results
-    except pymysql.OperationalError as e:
-        error_msg = f"Error occurred while fetching data for database '{database}': {e}"
-        print(error_msg)
-    except pymysql.Error as e:
-        error_msg = f"Unexpected error occurred while fetching data for database '{database}': {e}"
-        print(error_msg)
-
-    return []
+    return query(
+        database,
+        """
+        SELECT
+            DISTINCT a.actor_name AS username,
+            b.babel_lang as language_used,
+            b.babel_level as language_level
+        FROM actor a
+        LEFT JOIN babel b ON b.babel_user = a.actor_user
+        WHERE b.babel_user is not null
+        ORDER BY a.actor_name
+        """
+    )
 
 
 def format_language_proficiency(results, allowed_languages):
--- translation_stats/revision_table.py
+++ translation_stats/revision_table.py
@@ -1,26 +1,5 @@
-import pymysql
 import csv
-
-
-def make_connection(wiki, replica_type="analytics"):
-    """Connects to a host and database of the same name.
-    
-    `replica_type` can be either "analytics" (default) or "web"."""
-    assert replica_type == "web" or replica_type == "analytics"
-    return pymysql.connect(
-        host=f"{wiki}.{replica_type}.db.svc.wikimedia.cloud",
-        read_default_file="~/.my.cnf",
-        database=f"{wiki}_p",
-        charset='utf8'
-    )
-
-
-def query(conn, query):
-    """Execute a SQL query against the connection and return **all** the results."""
-    with conn.cursor() as cur:
-        cur.execute(query)
-        data = cur.fetchall()
-        return data
+from .wiki_replica import query
 
 
 def extract_db_names(file_path):
@@ -35,9 +14,8 @@ def extract_db_names(file_path):
 
 def process_database(wiki):
     try:
-        conn = make_connection(wiki)
         results = query(
-            conn,
+            wiki,
             """
             SELECT
                 r.rev_id as revision_id,
@@ -52,7 +30,6 @@ def process_database(wiki):
             LEFT JOIN comment c ON r.rev_comment_id = c.comment_id
             """
         )
-        conn.close()
 
         revision_ids = []
         usernames = []
@@ -83,10 +60,6 @@ def process_database(wiki):
             comments.append(comment)
 
         return revision_ids, usernames, revision_timestamps, source_wikis, target_wikis, comments
-
-    except pymysql.OperationalError as e:
-        print(f"Error occurred while connecting to the database: {e}")
-        return [], [], [], [], [], []
 
 
 def save_to_csv(file_path, revision_ids, usernames, revision_timestamps, source_wikis, target_wikis, comments):
